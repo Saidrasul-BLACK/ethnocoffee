77a86f7325467a8295c1f9ba55fa56a2eb4afb5f
Saidrasul-BLACK <saidrasulnew@gmail.com>
2025-04-04 22:18:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-from routes import users, menu, addresses
+from users import router as users_router
+from menu import router as menu_router
+from addresses import router as addresses_router
 
 app = FastAPI()
 
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(menu.router, prefix="/menu", tags=["Menu"])
-app.include_router(addresses.router, prefix="/addresses", tags=["Addresses"])
+app.include_router(users_router, prefix="/users")
+app.include_router(menu_router, prefix="/menu")
+app.include_router(addresses_router, prefix="/addresses")
+
 
 @app.get("/")
 def root():
