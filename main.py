bcab634af53685e6246f0c4ee482fdbede187b4f
Saidrasul-BLACK <saidrasulnew@gmail.com>
2025-04-04 22:36:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from users import router as users_router
 from menu import router as menu_router
 from addresses import router as addresses_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="uploads"), name="static"
 app.include_router(users_router, prefix="/users")
 app.include_router(menu_router, prefix="/menu")
 app.include_router(addresses_router, prefix="/addresses")
